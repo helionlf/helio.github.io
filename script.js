feb6e225771cdb51d0c8ef34562c183604a4b44c
Hélio Nogueira <116118382+helionlf@users.noreply.github.com>
2025-12-16 00:48:48 -0300

    refazendo design e adicionando contato por email

--- script.js
+++ script.js
@@ -1,3 +1,23 @@
+emailjs.init("MudNbBpwqBzqbawIS");
+
+document.getElementById("contact-form").addEventListener("submit", function(event) {
+  event.preventDefault();
+
+  const params = {
+    name: document.getElementById("name").value,
+    email: document.getElementById("email").value,
+    subject: document.getElementById("subject").value, 
+    message: document.getElementById("message").value
+  };
+
+  emailjs.send("service_ug69kur", "template_3bvwoda", params)
+    .then(() => {
+      document.getElementById("status").innerHTML = "Email enviado!";
+    }, () => {
+      document.getElementById("status").innerHTML = "Erro ao enviar.";
+    });
+});
+
 function baixar() {
     var urlCurriculo = "Currículo.pdf";
 
